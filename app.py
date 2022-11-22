68d9d10c4f4f93d814d2425e661c633b70d3f75c
Anton Topchii <anton.topchii@getbyrd.com>
2022-11-22 15:48:38 +0100

    Use feature flag

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import flask_config
+from flask_feature_flag import flag
 
 app = Flask(__name__)
 app.config.from_object(flask_config)
@@ -7,6 +8,8 @@ app.config.from_object(flask_config)
 
 @app.route("/")
 def hello_world():  # put application's code here
+    if flag("hello_world"):
+        return "Hello, World!"
     return "Meow!Meow!Meow!Meow!Meow!Meow!Meow!Meow!Meow!Meow!"
 
 
